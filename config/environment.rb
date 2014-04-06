81bcd4232e656b90e612a8338eaacf5d0bc7dd5f
frochat <frochat@gmail.com>
2014-04-06 10:03:54 +0000

    Chapter 13

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Depot::Application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:           587,
+    domain:         "gmail.com",
+    authentication: "plain",
+    user_name:      "fred_admin@etourdi.net",
+    password:       "zadministre",
+    enable_starttls_auto: true
+    }  
+end
+
